c42c2d3e0069ef9a12fc1299ed971fbfe527a9ed
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2026-01-10 13:04:06 -0500

    devops-137 - create JavaDoc comments for the starter framework.

--- src/main/java/org/fdu/App.java
+++ src/main/java/org/fdu/App.java
@@ -1,9 +1,18 @@
 package org.fdu;
 
 /**
- * Hello world!
+ * A console-based implementation of the classic Rock Paper Scissors game.
+ * This application allows a user to play against a computer opponent using
+ * standard win/loss logic.
  *
+ * <p>Standard rules apply: Rock beats Scissors, Scissors beats Paper,
+ * and Paper beats Rock.</p>
+ *
+ * @author many!
+ * @version 1.0.0
+ * @since 2026-01-10
  */
+
 public class App 
 {
     public static void main( String[] args ) {
--- src/main/java/org/fdu/RpsUi.java
+++ src/main/java/org/fdu/RpsUi.java
@@ -1,6 +1,21 @@
 package org.fdu;
 
+/**
+ * UI class - supports writing to console and reading user input and returning to caller
+ *
+ * <p>Stateless and content unaware.  The UI methods do not process the input or output</p>
+ *
+ * @author tbd
+ * @version 1.0.0
+ * @since 2026-01-10
+ */
+
 public class RpsUi {
+
+    /**
+     * Writes the provided String to the console.
+     * @param message - String to be output to the console
+     */
     public static void uiWriteMessage(String message) {
         System.out.println(message);
     }
