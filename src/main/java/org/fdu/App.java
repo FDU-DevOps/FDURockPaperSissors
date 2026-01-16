e7ffd03b95daeaf83e98906c1afd37792563627d
abehrensFDU <a.behrens@student.fdu.edu>
2026-01-16 17:07:53 -0500

    This is my extremely lazy RPS game.

--- src/main/java/org/fdu/App.java
+++ src/main/java/org/fdu/App.java
@@ -1,4 +1,5 @@
 package org.fdu;
+import java.util.Scanner;
 
 /**
  * A console-based implementation of the classic Rock Paper Scissors game.
@@ -15,7 +16,30 @@ package org.fdu;
 
 public class App 
 {
+    static void checkOutcome(int playerInput, int computerInput) {
+        if (playerInput == computerInput) {
+            RpsUi.uiWriteMessage( "Draw!");
+        }
+        else if (playerInput == 1 && computerInput == 3 || playerInput == 2 && computerInput == 1 || playerInput == 3 && computerInput == 2) {
+            RpsUi.uiWriteMessage( "You Win!");
+        }
+        else {
+            RpsUi.uiWriteMessage( "You Lose!");
+        }
+    }
     public static void main( String[] args ) {
-        RpsUi.uiWriteMessage( "Hello World!" );
+        Scanner scan = new Scanner(System.in);
+        RpsUi.uiWriteMessage( "Enter a number! \n 1. Rock \n 2. Paper \n 3. Scissors \n");
+        int playerInput;
+        int computerInput = (int)(Math.random() * 3) + 1;
+        try {
+            playerInput = scan.nextInt();
+            RpsUi.uiWriteMessage( "The computer chose " + computerInput + "!");
+            checkOutcome(playerInput, computerInput);
+        }
+        catch (Exception e) {
+            RpsUi.uiWriteMessage( "Invalid Input, Exiting...");
+        }
+        scan.close();
     }
 }
