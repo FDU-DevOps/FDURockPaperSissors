9bc0dc41011b3e3718402769047702ca3f0c396b
Xavier-Orrala <xavierorrala12404@gmail.com>
2026-01-19 16:37:44 -0500

    DEVOPS-163 Created a Scanner object and tested user input in Java for RPS game.

--- src/main/java/org/fdu/App.java
+++ src/main/java/org/fdu/App.java
@@ -1,4 +1,6 @@
 package org.fdu;
+import java.util.Scanner;
+
 
 /**
  * A console-based implementation of the classic Rock Paper Scissors game.
@@ -16,6 +18,16 @@ package org.fdu;
 public class App 
 {
     public static void main( String[] args ) {
-        RpsUi.uiWriteMessage( "Hello World!" );
+        RpsUi.uiWriteMessage( "Welcome to Xavier's RPS Game..." );
+        RpsUi.uiWriteMessage( "Enter 'R' for Rock, 'P' for Paper, and 'S' for Scissors...\n" );
+
+
+        Scanner RPS_SCANNER = new Scanner(System.in);
+        RpsUi.uiWriteMessage("Player enter your move: ");
+        String userTurn = RPS_SCANNER.nextLine();
+
+        //Testing user input
+        RpsUi.uiWriteMessage("USER INPUT: " + userTurn);
+
     }
 }
