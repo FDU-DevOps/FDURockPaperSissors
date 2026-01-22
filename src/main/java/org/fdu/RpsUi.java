3f1038c2f55d3e453ef6427a05a02782f8dc2da9
Lizst-cv <windy33e@gmail.com>
2026-01-21 20:36:11 -0500

    DEVOPS-165 Created Scanner and added a function to read the line in RpsUi

--- src/main/java/org/fdu/RpsUi.java
+++ src/main/java/org/fdu/RpsUi.java
@@ -1,7 +1,6 @@
 package org.fdu;
-
 /**
- * UI class - supports writing to console and reading user input and returning to caller
+ *UI class - supports writing to console and reading user input and returning to caller
  *
  * <p>Stateless and content unaware.  The UI methods do not process the input or output</p>
  *
@@ -9,8 +8,12 @@ package org.fdu;
  * @version 1.0.0
  * @since 2026-01-10
  */
+import java.util.Scanner;
 
 public class RpsUi {
+
+    //Create only one scanner to reuse it and read the lines
+    private static Scanner scanner = new Scanner(System.in);
 
     /** empty constructor - utility class - assumed all methods called statically */
     RpsUi() {    }
@@ -21,5 +24,20 @@ public class RpsUi {
      */
     public static void uiWriteMessage(String message) {
         System.out.println(message);
+    }
+
+    /**
+     * Reads a line and return it to the caller
+     * @return user input as an integer
+     */
+
+    public static int uiReadInput(){
+        while(!scanner.hasNext()) {
+            scanner.next();
+            uiWriteMessage("Invalid Input!");
+        }
+        int input = scanner.nextInt();
+        scanner.nextLine();
+        return input;
     }
 }
