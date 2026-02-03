eb45fd27199c8f9746594392f7d3efd6ced88282
Joshua Lynch <135474581+Joshua-Lynch@users.noreply.github.com>
2026-02-03 10:55:19 -0500

    DEVOPS-167 Made a simple RPS game

--- src/main/java/org/fdu/App.java
+++ src/main/java/org/fdu/App.java
@@ -1,4 +1,6 @@
 package org.fdu;
+import java.util.Scanner;
+import java.util.Random;
 
 /**
  * A console-based implementation of the classic Rock Paper Scissors game.
@@ -13,9 +15,39 @@ package org.fdu;
  * @since 2026-01-10
  */
 
-public class App 
+public class App
 {
-    public static void main( String[] args ) {
-        RpsUi.uiWriteMessage( "Hello World!" );
+    public static void main(String[] args)
+    {
+        System.out.println("Rock Paper Scissors:");
+
+
+        Scanner RPS_Scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.println("Press 1 for Rock, 2 for Scissors, 3 for Paper");
+        int playerChoice = RPS_Scanner.nextInt();
+
+        int computerChoice = random.nextInt(3) + 1;
+
+        if (playerChoice == computerChoice)
+        {
+            System.out.println("It's a tie!");
+        }
+        else if (
+                (playerChoice == 1 && computerChoice == 2) ||
+                        (playerChoice == 2 && computerChoice == 3) ||
+                        (playerChoice == 3 && computerChoice == 1)
+        )
+        {
+            System.out.println("You win!");
+        }
+        else
+        {
+            System.out.println("You lose!");
+        }
+
+        RPS_Scanner.close();
     }
 }
+
